68bf0431fdbd79d6be5df9868f4158f3df11f268
ong-ar <reclusis@gmail.com>
2019-04-03 01:11:42 +0900

    practice sticky header

--- src/Routes/Home/HomePresenter.tsx
+++ src/Routes/Home/HomePresenter.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
+import Header from "../../Components/Header";
 
 const HomePresenter: React.SFC = () => {
-  return <div>HOME</div>;
+  return (
+    <div style={{ height: "10000px" }}>
+      <Header topOffset={10} />
+    </div>
+  );
 };
 
 export default HomePresenter;
