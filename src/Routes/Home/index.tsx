3112c01e0f8a9d14bfa301b5d953e1df6b30bdcf
ong-ar <reclusis@gmail.com>
2019-04-07 19:44:01 +0900

    테스트를 위한 div 제거

--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,17 +1,20 @@
 import * as React from "react";
+import styled from "styled-components";
 import FirstSection from "../../Components/FirstSection";
 import Header from "../../Components/Header";
 import SecondSection from "../../Components/SecondSection";
 import StickyHeader from "../../Components/StickyHeader";
 
+const Container = styled.div``;
+
 const Home: React.FC = () => {
   return (
-    <div style={{ height: "2400px" }}>
+    <Container>
       <StickyHeader />
       <Header />
       <FirstSection />
       <SecondSection />
-    </div>
+    </Container>
   );
 };
 
